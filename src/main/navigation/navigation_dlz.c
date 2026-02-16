c6d6eb7ff645e21c15c53a907bdf5a1222f63f2e
m.strakl <m.strakl@axis-simulation.com>
2026-02-16 15:09:23 +0100

    (+) flag is volatile

--- src/main/navigation/navigation_dlz.c
+++ src/main/navigation/navigation_dlz.c
@@ -10,7 +10,7 @@
 #define UPDATE_TIMEOUT_MS 1000  // if no update in this time, DLZ is considered lost
 #define MAX_DIST 250
 
-static bool isNewDataReady = false;
+static volatile bool isNewDataReady = false;
 static uint32_t lastDataRx = 0;
 static navDlzData_t _mspData = {0};
 static navDlzData_t navDlzData = {0};
